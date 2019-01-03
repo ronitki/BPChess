7d4c2436ac3b08bad05515a748ccd5c956505c21
banuel <banuel@users.noreply.github.com>
2019-01-03 12:38:24 +0200

    Gui Start

--- src/main/java/il/ac/bgu/cs/bp/bpjs/Chess/BlackEventsListener.java
+++ src/main/java/il/ac/bgu/cs/bp/bpjs/Chess/BlackEventsListener.java
@@ -5,6 +5,8 @@ package il.ac.bgu.cs.bp.bpjs.Chess;
  */
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 import il.ac.bgu.cs.bp.bpjs.Chess.events.Move;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
@@ -14,12 +16,26 @@ import il.ac.bgu.cs.bp.bpjs.model.BEvent;
 
 public class BlackEventsListener extends BProgramRunnerListenerAdapter {
             @Override
-            public void eventSelected(BProgram bp, BEvent theEvent) {
+    Map<String,String> dicionary = new HashMap<>();
+
+    public BlackEventsListener() {
+        dicionary.put("1","a");
+        dicionary.put("2","b");
+        dicionary.put("3","c");
+        dicionary.put("4","d");
+        dicionary.put("5","e");
+        dicionary.put("6","f");
+        dicionary.put("7","g");
+    }
+
+    public void eventSelected(BProgram bp, BEvent theEvent) {
+
+
                 if (theEvent instanceof Move) {
                     Move mv = (Move) theEvent;
 
                     //   buttons[mv.row][mv.col].setText(mv.displayString());
-                    System.out.println(mv.toString());
+                    System.out.println(dicionary.get(mv.getSourceX()+"")+mv.getSourceY()+dicionary.get(mv.getTargetX()+"")+mv.getSourceY());
                 }
 //                } else {
 //                    if (theEvent == il.ac.bgu.cs.bp.bpjs.Chess.events.StaticEvents.XWin) {
--- src/main/java/il/ac/bgu/cs/bp/bpjs/Chess/GUIListener.java
+++ src/main/java/il/ac/bgu/cs/bp/bpjs/Chess/GUIListener.java
@@ -10,6 +10,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by בנואל on 03/01/2019.
@@ -17,11 +19,21 @@ import java.io.UnsupportedEncodingException;
 public class GUIListener implements Runnable {
     private BProgram bprog;
     private BufferedReader reader;
+    Map<String,String> dicionary = new HashMap<>();
     public GUIListener(BProgram bprog) throws UnsupportedEncodingException {
         this.bprog = bprog;
         reader = new BufferedReader(new InputStreamReader(System.in,"UTF-8"));
+        dicionaryInit();
     }
-
+    private void dicionaryInit(){
+        dicionary.put("a","1");
+        dicionary.put("b","2");
+        dicionary.put("c","3");
+        dicionary.put("d","4");
+        dicionary.put("e","5");
+        dicionary.put("f","6");
+        dicionary.put("g","7");
+    }
 
     @Override
     public void run() {
