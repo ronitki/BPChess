581495a1268fe4d55c4d2e42ce4e1b09256cd298
banuel <banuel@users.noreply.github.com>
2019-05-21 12:38:56 +0300

    Final Code

--- src/main/resources/assertions.js
+++ src/main/resources/assertions.js
@@ -1,10 +1,37 @@
-// CTX.subscribe("move to my own color", "CellWitColor", function(c){
-//     bp.sync({waitFor: Move.AllMovesThatTargretIs(c)});
-//     if(c.piece.color == My color)
-//     bp.ASSERT(false,"cell marked twice");
-// });
-//
-// bp.registerBThread("Assert X Win", function() {
-//     bp.sync({waitFor: bp.Event('XWin')});
-//     bp.ASSERT(false, "X won");
-// });
+
+bp.registerBThread("Wait 2 White", function() {
+    bp.sync({waitFor: Move.ColorMoveEventSet(Color.White)});
+    bp.sync({waitFor: Move.ColorMoveEventSet(Color.White)});
+    bp.ASSERT(false, "White Played Twice");
+});
+
+bp.registerBThread("Wait 2 Black", function() {
+    bp.sync({waitFor: Move.ColorMoveEventSet(Color.Black)});
+    bp.sync({waitFor: Move.ColorMoveEventSet(Color.Black)});
+    bp.ASSERT(false, "Black Played Twice");
+});
+
+bp.registerBThread("Wait Move in same place", function() {
+    bp.sync({waitFor: Move.SamePlaceMoveEventSet()});
+    bp.ASSERT(false, "Piece Moved in same black");
+});
+
+bp.registerBThread("Wait Move out of board", function() {
+    bp.sync({waitFor: Move.OutOfBoardMoveEventSet()});
+    bp.ASSERT(false, "Piece Moved out of board");
+});
+
+bp.registerBThread("Wait for eat my color", function() {
+    var move = bp.sync({waitFor: Move.AnyMoveEventSet()});
+    var target = (Cell)((Move)(move).target);
+    if (isNonEmpty(target)) {
+        var source = (Cell)((Move)(move).source);
+        if(getColor(source).equals(getColor(target))){
+            bp.ASSERT(false, "Ate Same Color");
+        }
+    }
+});
+
+
+
+
--- src/main/resources/verification.js
+++ src/main/resources/verification.js
@@ -1,5 +1,103 @@
-// Uci uci = new UCI();
-// CTX.subscribe("Simulate Move", "Cell", function(c){
-//     bp.sync({request: createEvent("Click",c)}, -20);
-// });
+importPackage(Packages.il.ac.bgu.cs.bp.bpjs.Chess.events);
+importPackage(Packages.il.ac.bgu.cs.bp.bpjs.context);
+importPackage(Packages.il.ac.bgu.cs.bp.bpjs.Chess.context.schema);
+importPackage(Packages.il.ac.bgu.cs.bp.bpjs.Chess.context.schema.piece);
 
+
+bp.registerBThread("init", function () {
+    var p1 = new Piece(Color.Black, Type.King, 1);
+    bp.sync({request: CTX.UpdateEvent("UpdateCell", {"cell": new Cell(4, 2, p1), "piece": p1})});
+    var p2 = new Piece(Color.Black, Type.Rook, 1);
+    bp.sync({request: CTX.UpdateEvent("UpdateCell", {"cell": new Cell(2,6, p2), "piece": p2})});
+    var p3 = new Piece(Color.White, Type.King, 1);
+    bp.sync({request: CTX.UpdateEvent("UpdateCell", {"cell": new Cell(2, 4, p3), "piece": p3})});
+    bp.sync({request: bp.Event("color", "black")});
+    bp.sync({request: bp.Event("init_end")});
+});
+//ask all 8 kings moves
+CTX.subscribe("AskMoveForKing-Test", "King", function (king) {
+    bp.sync({waitFor: bp.Event("Context Population Ended")});
+    bp.sync({waitFor: bp.Event("init_end")});
+    if (king.color.equals(Color.Black)) {
+        blackKing = king;
+    }
+    else {
+        whiteKing = king;
+    }
+    if (myColor.equals(Color.Black))
+        bp.sync({waitFor: bp.Event("EnginePlayed")});
+    while (true) {
+        var kingCell = getCellWithPiece(king);
+        if (kingCell === null)
+            break;
+        if (!isColor(kingCell, otherColor))
+            break;
+        var cells = [];
+        var currentCell;
+        var currentColor = getColor(kingCell);
+        cells.push(getCell(kingCell.i - 1, kingCell.j - 1));
+        cells.push(getCell(kingCell.i, kingCell.j - 1));
+        cells.push(getCell(kingCell.i + 1, kingCell.j - 1));
+        cells.push(getCell(kingCell.i - 1, kingCell.j));
+        cells.push(getCell(kingCell.i + 1, kingCell.j));
+        cells.push(getCell(kingCell.i - 1, kingCell.j + 1));
+        cells.push(getCell(kingCell.i, kingCell.j + 1));
+        cells.push(getCell(kingCell.i + 1, kingCell.j + 1));
+        var legalMoves = cells.map(
+            function (c) {
+                if (c !== "null")
+                    return new Move(kingCell, c, king);
+            });
+        var _legalMoves = legalMoves.filter(function (c) {
+            return c != null;
+        });
+        bp.sync({request: _legalMoves});
+        bp.sync({waitFor: bp.Event("My Color Played")});
+    }
+});
+//block all illegal kings moves
+CTX.subscribe("BlockIllegalMoveForKing-Test", "King", function (king) {
+    bp.sync({waitFor: bp.Event("Context Population Ended")});
+    bp.sync({waitFor: bp.Event("init_end")});
+    if (king.color.equals(Color.Black)) {
+        blackKing = king;
+    }
+    else {
+        whiteKing = king;
+    }
+    if (myColor.equals(Color.Black))
+        bp.sync({waitFor: bp.Event("EnginePlayed")});
+    while (true) {
+        var kingCell = getCellWithPiece(king);
+        if (kingCell === null)
+            break;
+        if (!isColor(kingCell, otherColor))
+            break;
+        var cellsToBlock = [];
+        var currentCell;
+        var currentColor = getColor(kingCell);
+        var i = kingCell.i;
+        var j = kingCell.j;
+
+        cellsToBlock.push(getCell(kingCell.i - 1, kingCell.j - 1));
+        cellsToBlock.push(getCell(kingCell.i, kingCell.j - 1));
+        cellsToBlock.push(getCell(kingCell.i + 1, kingCell.j - 1));
+        cellsToBlock.push(getCell(kingCell.i - 1, kingCell.j));
+        cellsToBlock.push(getCell(kingCell.i + 1, kingCell.j));
+        cellsToBlock.push(getCell(kingCell.i - 1, kingCell.j + 1));
+        cellsToBlock.push(getCell(kingCell.i, kingCell.j + 1));
+        cellsToBlock.push(getCell(kingCell.i + 1, kingCell.j + 1));
+
+        var IllegalcellsToBlock = cellsToBlock.map(
+            function (c) {
+                if ((c !== "null") && (!kingController(c, currentColor) || (isNonEmpty(c) && isColor(c, currentColor))))
+                    return new Move(kingCell, c, king);
+            });
+        var _IllegalcellsToBlock = IllegalcellsToBlock.filter(function (c) {
+            return c != null;
+        });
+
+        bp.sync({request: _legalMoves});
+        bp.sync({waitFor: bp.Event("My Color Played")});
+    }
+});
